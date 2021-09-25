53622f3cda2c3d849a8ef9b3a278fc475457d702
Scampish98 <38908094+Scampish98@users.noreply.github.com>
2021-09-25 15:47:03 +0300

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,53 @@
+function ElectricalAppliance(name, power) {
+  this.name = name;
+  this.power = power;
+  this.isPlugged = false;
+}
+  
+ElectricalAppliance.prototype.plugIn = function() {
+  console.log("Plug in " + this.name);
+  this.isPlugged = true;
+}
+ 
+ElectricalAppliance.prototype.plugOut = function() {
+  console.log("Plug out " + this.name);
+  this.isPlugged = false;
+}
+
+function Computer(name, power, brand, type) {
+  this.name = name;
+  this.power = power;
+  this.brand = brand;
+  this.type = type;
+}
+
+Computer.prototype = new ElectricalAppliance();
+
+function Lamp(name, power, color) {
+  this.name = name;
+  this.power = power;
+  this.color = color;
+}
+
+Lamp.prototype = new ElectricalAppliance();
+Lamp.prototype.changeColor = function(newColor) {
+  console.log('Change color for ' + this.name);
+  this.color = newColor;
+}
+
+computer = new Computer('my love', 1020, 'HP', 'gaming');
+console.log(computer);
+computer.plugIn();
+console.log(computer);
+computer.plugOut();
+console.log(computer);
+
+
+lamp = new Lamp('my lamp', 121, 'red');
+console.log(lamp);
+lamp.plugIn();
+console.log(lamp);
+lamp.plugOut();
+console.log(lamp);
+lamp.changeColor('yellow');
+console.log(lamp);
